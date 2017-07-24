76235a72dbf93a6a77241cc43b64ff9c203f8eda
Pedro Mass <pedro.j.mass@gmail.com>
2017-07-24 18:55:17 -0400

    Updated store to use the redux-extension

--- src/index.js
+++ src/index.js
@@ -8,8 +8,13 @@ import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import * as reducers from './store/reducers';
-const store = createStore(combineReducers(reducers), applyMiddleware(thunk));
+
+const store = createStore(
+  combineReducers(reducers),
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
